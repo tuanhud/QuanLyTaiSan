d319ce0f6b2c8425008dc286422b18d275459098
quocdunginfo <quocdunginfo@gmail.com>
2014-06-10 20:57:07 +0700

    Tạo thử Form thực hiện CRUD
    Mô hình không sử dụng lớp DAO (lớp trung gian truy xuất CSDL)
    Trong đó có kiểm tra ràng buộc và đồng bộ CSDL khi có sự thay đổi bên
    dưới. vd: xóa 1 obj không tồn tại do đã bị xóa ở nơi khác.

--- QuanLyTaiSan/Test_Form.Designer.cs
+++ QuanLyTaiSan/Test_Form.Designer.cs
@@ -28,12 +28,120 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button_refresh = new System.Windows.Forms.Button();
+            this.button_add = new System.Windows.Forms.Button();
+            this.textBox_permissionName = new System.Windows.Forms.TextBox();
+            this.button_remove = new System.Windows.Forms.Button();
+            this.button_update = new System.Windows.Forms.Button();
+            this.textBox_permissionNameEdit = new System.Windows.Forms.TextBox();
+            this.button_generateSample = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(246, 199);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button_refresh
+            // 
+            this.button_refresh.Location = new System.Drawing.Point(281, 12);
+            this.button_refresh.Name = "button_refresh";
+            this.button_refresh.Size = new System.Drawing.Size(106, 23);
+            this.button_refresh.TabIndex = 1;
+            this.button_refresh.Text = "Refresh/Load";
+            this.button_refresh.UseVisualStyleBackColor = true;
+            this.button_refresh.Click += new System.EventHandler(this.button_refresh_Click);
+            // 
+            // button_add
+            // 
+            this.button_add.Location = new System.Drawing.Point(281, 111);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(106, 23);
+            this.button_add.TabIndex = 2;
+            this.button_add.Text = "Add";
+            this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.button_add_Click);
+            // 
+            // textBox_permissionName
+            // 
+            this.textBox_permissionName.Location = new System.Drawing.Point(281, 85);
+            this.textBox_permissionName.Name = "textBox_permissionName";
+            this.textBox_permissionName.Size = new System.Drawing.Size(106, 20);
+            this.textBox_permissionName.TabIndex = 3;
+            // 
+            // button_remove
+            // 
+            this.button_remove.Location = new System.Drawing.Point(281, 47);
+            this.button_remove.Name = "button_remove";
+            this.button_remove.Size = new System.Drawing.Size(106, 23);
+            this.button_remove.TabIndex = 4;
+            this.button_remove.Text = "Remove";
+            this.button_remove.UseVisualStyleBackColor = true;
+            this.button_remove.Click += new System.EventHandler(this.button_remove_Click);
+            // 
+            // button_update
+            // 
+            this.button_update.Location = new System.Drawing.Point(281, 185);
+            this.button_update.Name = "button_update";
+            this.button_update.Size = new System.Drawing.Size(106, 23);
+            this.button_update.TabIndex = 5;
+            this.button_update.Text = "Update";
+            this.button_update.UseVisualStyleBackColor = true;
+            this.button_update.Click += new System.EventHandler(this.button_update_Click);
+            // 
+            // textBox_permissionNameEdit
+            // 
+            this.textBox_permissionNameEdit.Location = new System.Drawing.Point(281, 159);
+            this.textBox_permissionNameEdit.Name = "textBox_permissionNameEdit";
+            this.textBox_permissionNameEdit.Size = new System.Drawing.Size(106, 20);
+            this.textBox_permissionNameEdit.TabIndex = 6;
+            // 
+            // button_generateSample
+            // 
+            this.button_generateSample.Location = new System.Drawing.Point(446, 11);
+            this.button_generateSample.Name = "button_generateSample";
+            this.button_generateSample.Size = new System.Drawing.Size(74, 59);
+            this.button_generateSample.TabIndex = 7;
+            this.button_generateSample.Text = "Generate Sample Data";
+            this.button_generateSample.UseVisualStyleBackColor = true;
+            this.button_generateSample.Click += new System.EventHandler(this.button_generateSample_Click);
+            // 
+            // Test_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 288);
+            this.Controls.Add(this.button_generateSample);
+            this.Controls.Add(this.textBox_permissionNameEdit);
+            this.Controls.Add(this.button_update);
+            this.Controls.Add(this.button_remove);
+            this.Controls.Add(this.textBox_permissionName);
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.button_refresh);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Test_Form";
             this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Test_Form_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button_refresh;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.TextBox textBox_permissionName;
+        private System.Windows.Forms.Button button_remove;
+        private System.Windows.Forms.Button button_update;
+        private System.Windows.Forms.TextBox textBox_permissionNameEdit;
+        private System.Windows.Forms.Button button_generateSample;
     }
 }
 
--- QuanLyTaiSan/Test_Form.cs
+++ QuanLyTaiSan/Test_Form.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,21 +17,149 @@ namespace QuanLyTaiSan
         public Test_Form()
         {
             InitializeComponent();
-            //test Code first
-            MyDB db = new MyDB();
-            /*
-            foreach(ThietBi item in db.PHONGS.Where(p => p.id > 0).FirstOrDefault().thietbis)
+        }
+        private MyDB db;
+        private void button_refresh_Click(object sender, EventArgs e)
+        {
+            int prev_index = listBox1.SelectedIndex;
+            
+            Permission prev_obj=null;
+            if(prev_index>=0)
             {
+                prev_obj = ((List<Permission>)listBox1.DataSource).ElementAt(prev_index);
+            }
+            try
+            {
+
+                db = new MyDB();
+                listBox1.DisplayMember = "key";
+                List<Permission> objs = db.PERMISSIONS.ToList();
+                listBox1.DataSource = objs;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(msg_error_db_common);
+            }
+            finally
+            {
+                db.Dispose();
+            }
+
+            if (prev_obj!=null)
+            {
+                foreach(Permission tmp in ((List<Permission>)listBox1.DataSource))
+                {
+                    if (tmp.id == prev_obj.id)
+                    {
+                        listBox1.SelectedItem = tmp;
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void button_add_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                db = new MyDB();
+                Permission obj = new Permission { key = textBox_permissionName.Text, mota = textBox_permissionName.Text + "_mota" };
+                db.PERMISSIONS.Add(obj);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(msg_error_db_common);
+            }
+            finally
+            {
+                db.Dispose();//very importance, because of multiple-tracking exception
+                button_refresh.PerformClick();
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = listBox1.SelectedIndex;
+            //load to global var
+            editting_obj = ((List<Permission>)listBox1.DataSource).ElementAt(index);
+            textBox_permissionNameEdit.Text = editting_obj.key;
+        }
+        private Permission editting_obj;//current selected obj, item currently selected in listbox
+        private void button_update_Click(object sender, EventArgs e)
+        {
+            try{
+
+                db = new MyDB();
+                editting_obj.key = textBox_permissionNameEdit.Text;
+
+                db.PERMISSIONS.Attach(editting_obj);
+                db.Entry(editting_obj).State = EntityState.Modified;
+                db.SaveChanges();
                 
             }
-            
-            Group gp = db.GROUPS.Where(p => p.id > 0).FirstOrDefault();
-            foreach (Permission item in gp.permissions)
+            catch (Exception ex)
             {
-                if(item.isInGroup(gp))
-                Console.WriteLine(item.key);
+                MessageBox.Show(msg_db_obj_notfound);
             }
-            */
+            finally
+            {
+                db.Dispose();
+                button_refresh.PerformClick();
+            }
         }
+
+        private void button_remove_Click(object sender, EventArgs e)
+        {
+            //editting obj may be removed somewhere else
+            try
+            {
+                db = new MyDB();
+                db.PERMISSIONS.Attach(editting_obj);//must be done before remove
+                db.PERMISSIONS.Remove(editting_obj);
+                db.SaveChanges();
+                db.Dispose();
+
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(msg_db_obj_notfound);
+            }
+            finally
+            {
+                button_refresh.PerformClick();
+            }
+        }
+        private String msg_db_obj_notfound = "Obj not found, may be removed somewhere else";
+        private void Test_Form_Shown(object sender, EventArgs e)
+        {
+            button_refresh.PerformClick();
+        }
+
+        private void button_generateSample_Click(object sender, EventArgs e)
+        {
+            db = new MyDB();
+            for (int i = 0; i < 10; i++)
+            {
+                Permission obj = new Permission { key = "ex_" + i, mota = "nothing_" + i };
+                db.PERMISSIONS.Add(obj);
+            }
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(msg_error_db_common);
+            }
+            finally
+            {
+                db.Dispose();
+                button_refresh.PerformClick();
+            }
+        }
+        private String msg_error_db_common = "Database constrain fail!";
     }
 }
