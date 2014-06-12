a8598dcbef61f7ca23444c9967aaab5e7fa44945
khanhhc12 <khanh.hc12@gmail.com>
2014-06-12 18:55:42 +0700

    Revert "nothing"
    This reverts commit 53247e41b69f359e84920fbc808d6501d54953e2.

--- QuanLyTaiSan/Test_Area.cs
+++ QuanLyTaiSan/Test_Area.cs
@@ -86,7 +86,7 @@ namespace QuanLyTaiSan
             obj = obj.getById(1);
             obj.mota = "WTFWTWFWTWF";
             obj.update();
-            List<Dayy> day = obj.days.ToList();
+            List<Dayy> day = obj.dayys.ToList();
             Console.WriteLine(day[0].date_modified);
         }
     }
