878e4821421ab53fa2a3f4285fa28d0e139a1ce9
quocdunginfo <quocdunginfo@gmail.com>
2014-06-17 20:48:01 +0700

    Thêm hàm cho Entity VịTri
    Lấy danh sách vị trí theo coso, day, tang.

---
+++ QuanLyTaiSan/DataFilter/PhongFilter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyTaiSan.DataFilter
+{
+    class PhongFilter
+    {
+    }
+}
--- QuanLyTaiSan/Entities/ViTri.cs
+++ QuanLyTaiSan/Entities/ViTri.cs
@@ -34,5 +34,50 @@ namespace QuanLyTaiSan.Entities
         public virtual Tang tang { get; set; }
 
         public virtual ICollection<Phong> phongs { get; set; }
+        #region Nghiệp vụ
+        public List<ViTri> search(CoSo coso, Dayy day, Tang tang)
+        {
+            initDb();
+            List<ViTri> tmp = new List<ViTri>(); ;
+            if(coso==null && day==null && tang==null)
+            {
+                tmp = db.VITRIS.Where(c => c.coso==null && c.day==null && c.tang==null).ToList();
+            }
+            else if(coso!=null && day==null && tang==null)
+            {
+                tmp = db.VITRIS.Where(c => c.coso.id==coso.id && c.day==null && c.tang==null).ToList();
+            }
+            else if (coso != null && day != null && tang == null)
+            {
+                tmp = db.VITRIS.Where(c => c.coso.id == coso.id && c.day.id == day.id && c.tang == null).ToList();
+            }
+            else if (coso != null && day != null && tang != null)
+            {
+                tmp = db.VITRIS.Where(c => c.coso.id == coso.id && c.day.id == day.id && c.tang.id == tang.id).ToList();
+            }
+            if (tmp != null)
+            {
+                foreach (ViTri item in tmp)
+                {
+                    item.DB = db;
+                }
+            }
+            return tmp;
+        }
+        public List<ViTri> search(int coso_id, int day_id, int tang_id)
+        {
+            initDb();
+            List<ViTri> tmp = new List<ViTri>(); ;
+            CoSo coso = new CoSo();
+            coso = coso.getById(coso_id);
+            Dayy day = new Dayy();
+            day = day.getById(day_id);
+            Tang tang = new Tang();
+            tang = tang.getById(tang_id);
+            return search(coso, day, tang);
+        }
+
+
+        #endregion
     }
 }
