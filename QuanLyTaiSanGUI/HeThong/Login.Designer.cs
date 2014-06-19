71514cd7c631c3eebeb9a4dfc8791d2ac214ea61
xuongnhon <xuongnhon@gmail.com>
2014-06-19 16:02:27 +0700

    Đang code login

--- QuanLyTaiSanGUI/HeThong/Login.Designer.cs
+++ QuanLyTaiSanGUI/HeThong/Login.Designer.cs
@@ -34,6 +34,7 @@
             this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
             this.checkEdit_remember = new DevExpress.XtraEditors.CheckEdit();
             this.button_ok = new System.Windows.Forms.Button();
+            this.labelControl_msg = new DevExpress.XtraEditors.LabelControl();
             ((System.ComponentModel.ISupportInitialize)(this.textEdit_username.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.textEdit_password.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.checkEdit_remember.Properties)).BeginInit();
@@ -41,21 +42,27 @@
             // 
             // textEdit_username
             // 
-            this.textEdit_username.Location = new System.Drawing.Point(86, 24);
+            this.textEdit_username.Location = new System.Drawing.Point(86, 37);
             this.textEdit_username.Name = "textEdit_username";
             this.textEdit_username.Size = new System.Drawing.Size(180, 20);
             this.textEdit_username.TabIndex = 0;
+            this.textEdit_username.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textEdit_username_KeyPress);
+            this.textEdit_username.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textEdit_username_KeyUp);
             // 
             // textEdit_password
             // 
-            this.textEdit_password.Location = new System.Drawing.Point(86, 53);
+            this.textEdit_password.Location = new System.Drawing.Point(86, 66);
             this.textEdit_password.Name = "textEdit_password";
+            this.textEdit_password.Properties.PasswordChar = '•';
             this.textEdit_password.Size = new System.Drawing.Size(180, 20);
             this.textEdit_password.TabIndex = 1;
+            this.textEdit_password.EditValueChanged += new System.EventHandler(this.textEdit_password_EditValueChanged);
+            this.textEdit_password.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textEdit_password_KeyPress);
+            this.textEdit_password.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textEdit_password_KeyUp);
             // 
             // labelControl1
             // 
-            this.labelControl1.Location = new System.Drawing.Point(25, 27);
+            this.labelControl1.Location = new System.Drawing.Point(25, 40);
             this.labelControl1.Name = "labelControl1";
             this.labelControl1.Size = new System.Drawing.Size(46, 13);
             this.labelControl1.TabIndex = 2;
@@ -63,7 +70,7 @@
             // 
             // labelControl2
             // 
-            this.labelControl2.Location = new System.Drawing.Point(25, 56);
+            this.labelControl2.Location = new System.Drawing.Point(25, 69);
             this.labelControl2.Name = "labelControl2";
             this.labelControl2.Size = new System.Drawing.Size(44, 13);
             this.labelControl2.TabIndex = 3;
@@ -71,7 +78,7 @@
             // 
             // checkEdit_remember
             // 
-            this.checkEdit_remember.Location = new System.Drawing.Point(84, 79);
+            this.checkEdit_remember.Location = new System.Drawing.Point(84, 92);
             this.checkEdit_remember.Name = "checkEdit_remember";
             this.checkEdit_remember.Properties.Caption = "Ghi nhớ phiên";
             this.checkEdit_remember.Size = new System.Drawing.Size(182, 19);
@@ -80,18 +87,27 @@
             // button_ok
             // 
             this.button_ok.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button_ok.Location = new System.Drawing.Point(176, 103);
+            this.button_ok.Location = new System.Drawing.Point(176, 116);
             this.button_ok.Name = "button_ok";
             this.button_ok.Size = new System.Drawing.Size(90, 23);
             this.button_ok.TabIndex = 5;
             this.button_ok.Text = "Đăng nhập";
             this.button_ok.UseVisualStyleBackColor = true;
+            this.button_ok.Click += new System.EventHandler(this.button_ok_Click);
+            // 
+            // labelControl_msg
+            // 
+            this.labelControl_msg.Location = new System.Drawing.Point(87, 15);
+            this.labelControl_msg.Name = "labelControl_msg";
+            this.labelControl_msg.Size = new System.Drawing.Size(0, 13);
+            this.labelControl_msg.TabIndex = 6;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(293, 147);
+            this.ClientSize = new System.Drawing.Size(297, 169);
+            this.Controls.Add(this.labelControl_msg);
             this.Controls.Add(this.button_ok);
             this.Controls.Add(this.checkEdit_remember);
             this.Controls.Add(this.labelControl2);
@@ -100,6 +116,7 @@
             this.Controls.Add(this.textEdit_username);
             this.Name = "Login";
             this.Text = "Đăng nhập";
+            this.Load += new System.EventHandler(this.Login_Load);
             ((System.ComponentModel.ISupportInitialize)(this.textEdit_username.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.textEdit_password.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.checkEdit_remember.Properties)).EndInit();
@@ -116,5 +133,6 @@
         private DevExpress.XtraEditors.LabelControl labelControl2;
         private DevExpress.XtraEditors.CheckEdit checkEdit_remember;
         private System.Windows.Forms.Button button_ok;
+        private DevExpress.XtraEditors.LabelControl labelControl_msg;
     }
 }
--- QuanLyTaiSanGUI/HeThong/Login.cs
+++ QuanLyTaiSanGUI/HeThong/Login.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
+using QuanLyTaiSan.Entities;
 
 namespace QuanLyTaiSanGUI.HeThong
 {
@@ -16,6 +17,66 @@ namespace QuanLyTaiSanGUI.HeThong
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            //QuanTriVien obj = new QuanTriVien();
+            //obj.hoten = "Nguyen Dung";
+            //obj.username = "admin2";
+            //obj.hashPassword("admin2");
+            //obj.group = new Group(obj.DB).getById(1);
+            //obj.add();
+        }
+
+        private void button_ok_Click(object sender, EventArgs e)
+        {
+            QuanTriVien obj = new QuanTriVien();
+            obj.username = textEdit_username.Text;
+            obj.hashPassword(textEdit_password.Text);
+            
+            Boolean re = obj.checkLoginByUserName();
+            if (re)
+            {
+                labelControl_msg.Text = "Đăng nhập thành công!";
+                //...
+            }
+            else
+            {
+                labelControl_msg.Text = "Sai tài khoản hoặc mật khẩu!";
+
+            }
+        }
+
+        private void textEdit_username_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void textEdit_password_EditValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textEdit_password_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void textEdit_username_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button_ok.PerformClick();
+            }
+        }
+
+        private void textEdit_password_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button_ok.PerformClick();
+            }
         }
     }
 }
