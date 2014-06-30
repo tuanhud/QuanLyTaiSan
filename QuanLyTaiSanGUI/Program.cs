f21c0a104cd23eb7b8799cba818c2222f8bc0648
nguyenhoangthanh <nguyenhoangthanhsgu@gmail.com>
2014-06-30 17:48:25 +0700

    đang làm form newthietbi

--- QuanLyTaiSanGUI/Program.cs
+++ QuanLyTaiSanGUI/Program.cs
@@ -26,7 +26,8 @@ namespace QuanLyTaiSanGUI
             //Application.Run(new Form1());
             //Application.Run(new frmHinhAnh());
             //Application.Run(new frmThuVienHinhAnh());
-            Application.Run(new frmMain());
+            //Application.Run(new frmMain());
+            Application.Run(new QuanLyTaiSanGUI.MyForm.frmNewThietBi());
             //Application.Run(new Setting());
             //Application.Run(new SplashScreen1());
         }
