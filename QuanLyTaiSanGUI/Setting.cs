03a6bc74fa469d3e065927cc2883e0dde8452167
satthuit <satthucntt@gmail.com>
2014-07-01 14:36:24 +0700

    thêm hàm check kết nối FTP trong class FTPHelper

--- QuanLyTaiSanGUI/Setting.cs
+++ QuanLyTaiSanGUI/Setting.cs
@@ -31,6 +31,11 @@ namespace QuanLyTaiSanGUI
             this.cau_hinh_ban_dau = cauHinhBanDau;
         }
 
+        private void Setting_Load(object sender, EventArgs e)
+        {
+            btnCauHinh.PerformClick();
+        }
+
         private void btnCauHinh_Click(object sender, EventArgs e)
         {
             if (_ucCauHinh == null)
