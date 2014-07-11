1b0eb74be2a7eb4857fc7802869538517adfb29f
satthuit <satthucntt@gmail.com>
2014-07-11 19:55:10 +0700

    fix QuanLyTaiSan + WebQLTS
    Đã upload thành công lên host qlts.hoangthanhit.com
    Build QLTS thành file dll
    => nếu để file config trong QLTS sẽ bulid thành dll, giải quyết đc vấn
    đề lộ config
    Dũng suy nghĩ thêm :dancer:

--- QuanLyTaiSan/Program.cs
+++ QuanLyTaiSan/Program.cs
@@ -17,7 +17,7 @@ namespace QuanLyTaiSan
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Test_Form_2());
-            Application.Run(new Test_Area());
+            //Application.Run(new Test_Area());
         }
     }
 }
