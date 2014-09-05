074d6b6a0c4f2f0557a0815d860940dc3b1a2eb7
khanhhc12 <khanh.hc12@gmail.com>
2014-09-05 13:49:58 +0700

    nothing

--- TSCD_GUI/QLTaiSan/ucQuanLyDonVi_TaiSan.Designer.cs
+++ TSCD_GUI/QLTaiSan/ucQuanLyDonVi_TaiSan.Designer.cs
@@ -36,8 +36,17 @@
             this.rbnGroupTaiSan = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
             this.navBarControlLeft = new DevExpress.XtraNavBar.NavBarControl();
             this.navBarGroupDonVi = new DevExpress.XtraNavBar.NavBarGroup();
+            this.gridControlDonVi_TaiSan = new DevExpress.XtraGrid.GridControl();
+            this.gridViewDonVi_TaiSan = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.navBarGroupControlContainerDonVi = new DevExpress.XtraNavBar.NavBarGroupControlContainer();
+            this.treeListDonVi = new DevExpress.XtraTreeList.TreeList();
             ((System.ComponentModel.ISupportInitialize)(this.rbnControlDonVi_TaiSan)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.navBarControlLeft)).BeginInit();
+            this.navBarControlLeft.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControlDonVi_TaiSan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewDonVi_TaiSan)).BeginInit();
+            this.navBarGroupControlContainerDonVi.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.treeListDonVi)).BeginInit();
             this.SuspendLayout();
             // 
             // rbnControlDonVi_TaiSan
@@ -92,6 +101,7 @@
             // navBarControlLeft
             // 
             this.navBarControlLeft.ActiveGroup = this.navBarGroupDonVi;
+            this.navBarControlLeft.Controls.Add(this.navBarGroupControlContainerDonVi);
             this.navBarControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
             this.navBarControlLeft.Groups.AddRange(new DevExpress.XtraNavBar.NavBarGroup[] {
             this.navBarGroupDonVi});
@@ -106,19 +116,60 @@
             // navBarGroupDonVi
             // 
             this.navBarGroupDonVi.Caption = "Đơn vị";
+            this.navBarGroupDonVi.ControlContainer = this.navBarGroupControlContainerDonVi;
             this.navBarGroupDonVi.Expanded = true;
+            this.navBarGroupDonVi.GroupClientHeight = 80;
+            this.navBarGroupDonVi.GroupStyle = DevExpress.XtraNavBar.NavBarGroupStyle.ControlContainer;
             this.navBarGroupDonVi.Name = "navBarGroupDonVi";
+            // 
+            // gridControlDonVi_TaiSan
+            // 
+            this.gridControlDonVi_TaiSan.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControlDonVi_TaiSan.Location = new System.Drawing.Point(187, 142);
+            this.gridControlDonVi_TaiSan.MainView = this.gridViewDonVi_TaiSan;
+            this.gridControlDonVi_TaiSan.MenuManager = this.rbnControlDonVi_TaiSan;
+            this.gridControlDonVi_TaiSan.Name = "gridControlDonVi_TaiSan";
+            this.gridControlDonVi_TaiSan.Size = new System.Drawing.Size(674, 382);
+            this.gridControlDonVi_TaiSan.TabIndex = 3;
+            this.gridControlDonVi_TaiSan.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridViewDonVi_TaiSan});
+            // 
+            // gridViewDonVi_TaiSan
+            // 
+            this.gridViewDonVi_TaiSan.GridControl = this.gridControlDonVi_TaiSan;
+            this.gridViewDonVi_TaiSan.Name = "gridViewDonVi_TaiSan";
+            // 
+            // navBarGroupControlContainerDonVi
+            // 
+            this.navBarGroupControlContainerDonVi.Controls.Add(this.treeListDonVi);
+            this.navBarGroupControlContainerDonVi.Name = "navBarGroupControlContainerDonVi";
+            this.navBarGroupControlContainerDonVi.Size = new System.Drawing.Size(187, 279);
+            this.navBarGroupControlContainerDonVi.TabIndex = 0;
+            // 
+            // treeListDonVi
+            // 
+            this.treeListDonVi.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeListDonVi.Location = new System.Drawing.Point(0, 0);
+            this.treeListDonVi.Name = "treeListDonVi";
+            this.treeListDonVi.Size = new System.Drawing.Size(187, 279);
+            this.treeListDonVi.TabIndex = 0;
             // 
             // ucQuanLyDonVi_TaiSan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gridControlDonVi_TaiSan);
             this.Controls.Add(this.navBarControlLeft);
             this.Controls.Add(this.rbnControlDonVi_TaiSan);
             this.Name = "ucQuanLyDonVi_TaiSan";
             this.Size = new System.Drawing.Size(861, 524);
             ((System.ComponentModel.ISupportInitialize)(this.rbnControlDonVi_TaiSan)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.navBarControlLeft)).EndInit();
+            this.navBarControlLeft.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControlDonVi_TaiSan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewDonVi_TaiSan)).EndInit();
+            this.navBarGroupControlContainerDonVi.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.treeListDonVi)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -134,5 +185,9 @@
         private DevExpress.XtraBars.BarButtonItem barBtnThemTaiSan;
         private DevExpress.XtraBars.BarButtonItem barBtnSuaTaiSan;
         private DevExpress.XtraBars.BarButtonItem barBtnXoaTaiSan;
+        private DevExpress.XtraGrid.GridControl gridControlDonVi_TaiSan;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridViewDonVi_TaiSan;
+        private DevExpress.XtraNavBar.NavBarGroupControlContainer navBarGroupControlContainerDonVi;
+        private DevExpress.XtraTreeList.TreeList treeListDonVi;
     }
 }
---
+++ TSCD_GUI/QLTaiSan/ucQuanLyTaiSan.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace TSCD_GUI.QLTaiSan
+{
+    partial class ucQuanLyTaiSan
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbnControlTaiSan = new DevExpress.XtraBars.Ribbon.RibbonControl();
+            this.rbnPageTaiSan = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.rbnGroupTaiSan = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+            this.barBtnThemTaiSan = new DevExpress.XtraBars.BarButtonItem();
+            this.barBtnSuaTaiSan = new DevExpress.XtraBars.BarButtonItem();
+            this.barBtnXoaTaiSan = new DevExpress.XtraBars.BarButtonItem();
+            this.gridControlTaiSan = new DevExpress.XtraGrid.GridControl();
+            this.gridViewTaiSan = new DevExpress.XtraGrid.Views.Grid.GridView();
+            ((System.ComponentModel.ISupportInitialize)(this.rbnControlTaiSan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControlTaiSan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewTaiSan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // rbnControlTaiSan
+            // 
+            this.rbnControlTaiSan.ExpandCollapseItem.Id = 0;
+            this.rbnControlTaiSan.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.rbnControlTaiSan.ExpandCollapseItem,
+            this.barBtnThemTaiSan,
+            this.barBtnSuaTaiSan,
+            this.barBtnXoaTaiSan});
+            this.rbnControlTaiSan.Location = new System.Drawing.Point(0, 0);
+            this.rbnControlTaiSan.MaxItemId = 5;
+            this.rbnControlTaiSan.Name = "rbnControlTaiSan";
+            this.rbnControlTaiSan.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.rbnPageTaiSan});
+            this.rbnControlTaiSan.Size = new System.Drawing.Size(858, 142);
+            // 
+            // rbnPageTaiSan
+            // 
+            this.rbnPageTaiSan.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.rbnGroupTaiSan});
+            this.rbnPageTaiSan.Name = "rbnPageTaiSan";
+            this.rbnPageTaiSan.Text = "Tài sản";
+            // 
+            // rbnGroupTaiSan
+            // 
+            this.rbnGroupTaiSan.ItemLinks.Add(this.barBtnThemTaiSan);
+            this.rbnGroupTaiSan.ItemLinks.Add(this.barBtnSuaTaiSan);
+            this.rbnGroupTaiSan.ItemLinks.Add(this.barBtnXoaTaiSan);
+            this.rbnGroupTaiSan.Name = "rbnGroupTaiSan";
+            this.rbnGroupTaiSan.ShowCaptionButton = false;
+            this.rbnGroupTaiSan.Text = "Tài sản";
+            // 
+            // barBtnThemTaiSan
+            // 
+            this.barBtnThemTaiSan.Caption = "Thêm tài sản";
+            this.barBtnThemTaiSan.Glyph = global::TSCD_GUI.Properties.Resources.plus_2;
+            this.barBtnThemTaiSan.Id = 1;
+            this.barBtnThemTaiSan.Name = "barBtnThemTaiSan";
+            // 
+            // barBtnSuaTaiSan
+            // 
+            this.barBtnSuaTaiSan.Caption = "Sửa tài sản";
+            this.barBtnSuaTaiSan.Glyph = global::TSCD_GUI.Properties.Resources.pencil_edit;
+            this.barBtnSuaTaiSan.Id = 2;
+            this.barBtnSuaTaiSan.Name = "barBtnSuaTaiSan";
+            // 
+            // barBtnXoaTaiSan
+            // 
+            this.barBtnXoaTaiSan.Caption = "Xóa tài sản";
+            this.barBtnXoaTaiSan.Glyph = global::TSCD_GUI.Properties.Resources.minus_2;
+            this.barBtnXoaTaiSan.Id = 3;
+            this.barBtnXoaTaiSan.Name = "barBtnXoaTaiSan";
+            // 
+            // gridControlTaiSan
+            // 
+            this.gridControlTaiSan.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControlTaiSan.Location = new System.Drawing.Point(0, 142);
+            this.gridControlTaiSan.MainView = this.gridViewTaiSan;
+            this.gridControlTaiSan.MenuManager = this.rbnControlTaiSan;
+            this.gridControlTaiSan.Name = "gridControlTaiSan";
+            this.gridControlTaiSan.Size = new System.Drawing.Size(858, 367);
+            this.gridControlTaiSan.TabIndex = 1;
+            this.gridControlTaiSan.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridViewTaiSan});
+            // 
+            // gridViewTaiSan
+            // 
+            this.gridViewTaiSan.GridControl = this.gridControlTaiSan;
+            this.gridViewTaiSan.Name = "gridViewTaiSan";
+            // 
+            // ucQuanLyTaiSan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gridControlTaiSan);
+            this.Controls.Add(this.rbnControlTaiSan);
+            this.Name = "ucQuanLyTaiSan";
+            this.Size = new System.Drawing.Size(858, 509);
+            ((System.ComponentModel.ISupportInitialize)(this.rbnControlTaiSan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControlTaiSan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridViewTaiSan)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.Ribbon.RibbonControl rbnControlTaiSan;
+        private DevExpress.XtraBars.Ribbon.RibbonPage rbnPageTaiSan;
+        private DevExpress.XtraBars.Ribbon.RibbonPageGroup rbnGroupTaiSan;
+        private DevExpress.XtraBars.BarButtonItem barBtnThemTaiSan;
+        private DevExpress.XtraBars.BarButtonItem barBtnSuaTaiSan;
+        private DevExpress.XtraBars.BarButtonItem barBtnXoaTaiSan;
+        private DevExpress.XtraGrid.GridControl gridControlTaiSan;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridViewTaiSan;
+    }
+}
