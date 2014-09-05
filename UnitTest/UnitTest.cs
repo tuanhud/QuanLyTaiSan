c268e9a3e0bdf4e3293b71871249b9ef7429a8ea
quocdunginfo <quocdunginfo@gmail.com>
2014-09-05 22:46:38 +0700

    Thêm Project UnitTest vào cho màu mè hoa lá hẹ
    Mục đích của project là để kiểm thử tự động một vài thứ trong solution
    Lúc báo cáo cũng có cái hay ho để múa
    Không ảnh hưởng gì đến hệ thống hiện tại

---
+++ UnitTest/UnitTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SHARED.Libraries;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class Libraries
+    {
+        [TestMethod]
+        public void Test_SHA1()
+        {
+            string expected = "d033e22ae348aeb5660fc2140aec35850c4da997";
+            String actual = StringHelper.SHA1("admin").ToLower();
+            Assert.AreEqual(expected,actual);
+        }
+        [TestMethod]
+        public void Test_CoDauKhongDau()
+        {
+            string expected = "ban di choi. CHUNG TOI O LAI~!@#$%^&*()_+";
+            String actual = StringHelper.CoDauThanhKhongDau("bạn đi chơi. CHÚNG TÔI Ở LẠI~!@#$%^&*()_+");
+            Assert.AreEqual(expected, actual);
+
+            Assert.AreEqual("", StringHelper.CoDauThanhKhongDau(""));
+        }
+        [TestMethod]
+        public void Test_DateTimeToMiliSec()
+        {
+            Assert.AreEqual(true, DateTimeHelper.toMilisec(new DateTime(2014,1,1,1,1,1))>0);
+        }
+    }
+}
