bcf194bfc3d91330a547c5e86a6e5ed87c798aa2
khanhhc12 <khanh.hc12@gmail.com>
2014-09-13 14:29:11 +0700

    nothing

--- TSCD/DataFilter/TaiSanHienThi.cs
+++ TSCD/DataFilter/TaiSanHienThi.cs
@@ -187,5 +187,44 @@ namespace TSCD.DataFilter
                 return null;
             }
         }
+
+        public static List<TaiSanHienThi> Convert(IQueryable<CTTaiSan> list)
+        {
+            try
+            {
+                if (list == null)
+                    return null;
+                List<TaiSanHienThi> re =
+                list.Select(ct => new TaiSanHienThi
+                {
+                    id = ct.id,
+                    ngayghi = ct.ngay,
+                    sohieu_ct = ct.chungtu_sohieu,
+                    ngay_ct = ct.chungtu_ngay,
+                    ten = ct.taisan.ten,
+                    loaits = ct.taisan.loaitaisan.ten,
+                    donvitinh = ct.taisan.loaitaisan.donvitinh.ten,
+                    soluong = ct.soluong,
+                    dongia = ct.taisan.dongia,
+                    thanhtien = ct.soluong * ct.taisan.dongia,
+                    nguongoc = ct.nguongoc,
+                    tinhtrang = ct.tinhtrang.value,
+                    ghichu = ct.mota,
+                    childs = ct.childs,
+                    phong = ct.phong != null ? ct.phong.ten : "",
+                    vitri = ct.vitri != null ? (ct.vitri.coso != null ? ct.vitri.coso.ten + (ct.vitri.day != null ? " - " +
+                    ct.vitri.day.ten + (ct.vitri.tang != null ? " - " + ct.vitri.tang.ten : "") : "") : "") : "",
+                    dvquanly = ct.donviquanly != null ? ct.donviquanly.ten : "",
+                    dvsudung = ct.donvisudung != null ? ct.donvisudung.ten : "",
+                    obj = ct,
+                }).ToList();
+                return re;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("TaiSanHienThi->Convert:" + ex.Message);
+                return null;
+            }
+        }
     }
 }
