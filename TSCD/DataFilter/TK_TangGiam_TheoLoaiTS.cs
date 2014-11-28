48ef6a0472419d3acd1a5f7c6e42ef70d51e1da4
Nguyễn Hoàng Thanh <nguyenhoangthanhsgu@gmail.com>
2014-11-28 18:10:06 +0700

    nothing

--- TSCD/DataFilter/TK_TangGiam_TheoLoaiTS.cs
+++ TSCD/DataFilter/TK_TangGiam_TheoLoaiTS.cs
@@ -32,7 +32,7 @@ namespace TSCD.DataFilter
         /// <returns></returns>
         public static List<TK_TangGiam_TheoLoaiTS> getAll(DateTime? from, DateTime? to)
         {
-            var list_loaits = LoaiTaiSan.getAll();// LoaiTaiSan.getAll();
+            var list_loaits = LoaiTaiSan.getAll();
             List<TK_TangGiam_TheoLoaiTS> re = new List<TK_TangGiam_TheoLoaiTS>();
 
             foreach (var item in list_loaits)
@@ -58,28 +58,28 @@ namespace TSCD.DataFilter
                         obj.sodaunam_soluong += ctts.soluong;
                         obj.sodaunam_giatri += ctts.thanhtien;
                     }
-                    //var tmp = taisan.cttaisans.Where(
-                    //    c =>
-                    //        c.soluong > 0
-                    //        &&
-                    //        (
-                    //            (c.ngay != null && c.ngay < from)
-                    //        )
-                    //    );
-                    //obj.sodaunam_soluong = tmp.Sum(x => x.soluong);
-                    //obj.sodaunam_giatri = tmp.Sum(x => x.thanhtien);
+                    var tmp = taisan.cttaisans.Where(
+                        c =>
+                            c.soluong > 0
+                            &&
+                            (
+                                (c.ngay != null && c.ngay < from)
+                            )
+                        );
+                    obj.sodaunam_soluong = tmp.Sum(x => x.soluong);
+                    obj.sodaunam_giatri = tmp.Sum(x => x.thanhtien);
 
 
                     var list_log_ctts = taisan.logtanggiamtaisans.Where(c => c.date_create != null && c.date_create >= from && c.date_create <= to && c.soluong > 0);
                     Console.WriteLine("quocdunginfo: " + count++);
 
-                    //var tmp_giam = list_log_ctts.Where(x => x.tang_giam == -1);
-                    //obj.giamtrongnam_soluong = tmp_giam.Sum(x => x.soluong);
-                    //obj.giamtrongnam_giatri = tmp_giam.Sum(x => x.thanhtien);
+                    var tmp_giam = list_log_ctts.Where(x => x.tang_giam == -1);
+                    obj.giamtrongnam_soluong = tmp_giam.Sum(x => x.soluong);
+                    obj.giamtrongnam_giatri = tmp_giam.Sum(x => x.thanhtien);
 
-                    //var tmp_tang = list_log_ctts.Where(x => x.tang_giam == 1);
-                    //obj.tangtrongnam_soluong = tmp_giam.Sum(x => x.soluong);
-                    //obj.tangtrongnam_giatri = tmp_giam.Sum(x => x.thanhtien);
+                    var tmp_tang = list_log_ctts.Where(x => x.tang_giam == 1);
+                    obj.tangtrongnam_soluong = tmp_giam.Sum(x => x.soluong);
+                    obj.tangtrongnam_giatri = tmp_giam.Sum(x => x.thanhtien);
                     //Tính tăng/giảm trong năm
                     foreach (var ctts in list_log_ctts.Where(c => c.tang_giam == 1 || c.tang_giam == -1))
                     {
